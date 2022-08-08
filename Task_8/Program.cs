b71b82ad4effa713bd29c2e69a0eae9e84be652a
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-08 16:46:52 +0400

    solved the problem

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.WriteLine("Enter some number: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int i = 1;
+int i = 0;
 
-if (i <= n) {
-    if (i%2 == 0) {
+while (i <= n) {
+    if (i % 2 == 0) {
         Console.WriteLine(i);
         i++;
     }
