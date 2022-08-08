212db1eb75f7f415365ed7ae367b58c7e42495e4
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-08 09:58:18 +0400

    Zadacha_4 added

---
+++ Task_4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Enter number A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number C: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a >= b || a >= c) {
+ Console.Write("The max number is: ");
+ Console.WriteLine(a);
+}
+
+ else if (b >= a || b >= c) {
+ Console.Write("The max number is: ");
+ Console.WriteLine(b);
+}
+
+ else if (c >= a || c >= b) {
+ Console.Write("The max number is: ");
+ Console.WriteLine(c);
+}
+
+else {
+ Console.WriteLine("Incorrect number.");
+}
