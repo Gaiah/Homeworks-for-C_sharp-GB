c6c1d3897827800c9f0564ec8061d24967e9938f
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-08 16:20:06 +0400

    Task_8 added

---
+++ Task_8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter some number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+if (i <= n) {
+    if (i%2 == 0) {
+        Console.WriteLine(i);
+        i++;
+    }
+  
+ else {
+    i++;
+  }
+}
