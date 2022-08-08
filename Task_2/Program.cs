18209478b97de59f6aee485229be617d1f47fda0
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-08 09:47:59 +0400

    zadacha_2 added

---
+++ Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Input number A: "); 
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Input number B: "); 
+int b = Convert.ToInt32(Console.ReadLine()); 
+int max; 
+ 
+if(a > b) { 
+    max = a; 
+    Console.WriteLine("The max number is: "); 
+    Console.WriteLine(max); 
+} 
+ 
+else if(a < b) { 
+    max = b; 
+    Console.WriteLine("The max number is: "); 
+    Console.WriteLine(max); 
+} 
+ 
+else if(a == b) { 
+ Console.WriteLine("These two numbers are equal!"); 
+} 
+ 
+else { 
+ Console.WriteLine("Oops... Looks like something went wrong. Try again with correct numbers!"); 
+}
