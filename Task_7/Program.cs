ce4a3038ecbe18b30dcff7a5f445fa3d3ed6ddd9
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-08 18:23:43 +0400

    Task_7 added

---
+++ Task_7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter a three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 99 || num > 999) {
+    Console.WriteLine("Incorrect number.");
+}
+
+else {
+    Console.WriteLine("The last digit of Your number is: ");
+    Console.WriteLine(num % 10);
+}
