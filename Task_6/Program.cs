153d11e9c66164aff1b1dd6cb076377911519dac
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-08 10:06:54 +0400

    Zadacha_6 added

---
+++ Task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0) {
+ Console.Write("This number is even: ");
+ Console.WriteLine(num);
+}
+
+else {
+ Console.Write("This number isn't even: ");
+ Console.WriteLine(num);
+}
