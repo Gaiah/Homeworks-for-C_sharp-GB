0d0093f65555ad146bd675a39d49b49052c0dff5
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-08 18:09:36 +0400

    Task_5 added

---
+++ Task_5/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+if (n < 0) {
+    n = n * -1;
+}
+
+int count = n * 2 + 1;
+n = n * -1;
+
+while (i < count) {
+    Console.WriteLine(n);
+    n++;
+    i++;
+}
